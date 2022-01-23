a75681281bf85c673e6f06052bacf196af22fbaf
unknown <upnimu@gmail.com>
2022-01-23 21:23:09 +0530

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fetch=require("node-fetch");
-var IP=""; // Enter your desired IP address here
+var IP="76.67.74.227"; // Enter your desired IP address here
 fetch(`http://ip-api.com/json/${IP}`)
     .then(res=>res.json())
     .then(json=>console.log(json))
